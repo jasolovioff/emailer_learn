3e544768b51f203056fd13c83eeb38503c495cb7
jorge <jorge@imatec.cl>
2020-06-29 21:14:31 -0400

    Survey creation on client side ready.

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    {label: "Survey Title", name: "title", error: "You have to provide a title"},
+    {label: "Subject Line", name: "subject", error: "You have to provide a subject"},
+    {label: "Email Body", name: "body", error: "You have to provide a email body"},
+    {label: "Recipient List", name: "recipients", error: "You have to provide at least one email"}
+]
